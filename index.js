5409118fc48462caf626646e4dfd330c6a1b1881
Shone Binu <shonebinualias@gmail.com>
2022-10-16 12:59:40 +0530

    First To-do complete

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let grid_size = 16;
+
+const container = document.querySelector('#grid-container');
+
+const putSquare = (grid_size) => {
+    for (let i = 0; i < grid_size; i++) {
+        const newHorizontal = document.createElement('div');
+        newHorizontal.style.cssText = `width: 100%; height: ${70/grid_size}vh; display: flex;`
+        container.appendChild(newHorizontal);
+        for (let j = 0; j < grid_size; j++) {
+            const newTinySquare = document.createElement('div');
+            newTinySquare.style.cssText = `height: 100%; width: ${70/grid_size}vh;`
+            newTinySquare.classList.add('tinySquare')
+            newHorizontal.appendChild(newTinySquare);
+        };
+    };
+};
+
+const tinySquare = document.querySelectorAll('.tinySquare');
+tinySquare.forEach(square => {
+    
+});
+
+
+
+putSquare(grid_size);
+
